f4e0668033742da5692c4964894c27b50f7143a0
Waseem Samkari <wsamkari2022@my.fit.edu>
2025-05-30 21:04:58 -0400

    Design and UX Improvements

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,18 @@ import PreferencesPage from './implicit_value_pages/PreferencesPage';
 import CompletionPage from './implicit_value_pages/CompletionPage';
 import ValuesPage from './implicit_value_pages/ValuesPage';
 import SimulationPage from './SimulationMainPage';
-import AdaptivePreferencePage from './pages/AdaptivePreferencePage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/demographics\" replace />} />
+        <Route path="/" element={<Navigate to="/demographics" replace />} />
         <Route path="/demographics" element={<DemographicPage />} />
         <Route path="/explicitvaluepage" element={<ExplicitValuesPage />} />
         <Route path="/preferences" element={<PreferencesPage />} />
         <Route path="/completion" element={<CompletionPage />} />
         <Route path="/values" element={<ValuesPage />} />
         <Route path="/simulation" element={<SimulationPage />} />
-        <Route path="/adaptive-preference" element={<AdaptivePreferencePage />} />
       </Routes>
     </Router>
   );
